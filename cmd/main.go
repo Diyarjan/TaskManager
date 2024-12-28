7274870f9b3a3daa02bdf304cca249aa636a7c4e
Diyarjan <diyarjan.friend.dm@gmail.com>
2024-12-29 00:56:39 +0500

    refactoring

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,41 @@
 package main
 
 import (
-	"log"
-
 	task "github.com/Diyarjan/TasksManager"
 	"github.com/Diyarjan/TasksManager/pkg/handler"
 	"github.com/Diyarjan/TasksManager/pkg/repository"
 	"github.com/Diyarjan/TasksManager/pkg/service"
+	_ "github.com/lib/pq"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
 func main() {
+	logrus.SetFormatter(new(logrus.JSONFormatter))
 
 	if err := initConfig(); err != nil {
-		log.Fatalf("error initializing configs: %s", err.Error())
+		logrus.Fatalf("error initializing configs: %s", err.Error())
 	}
 
-	repos := repository.NewRepository()
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: viper.GetString("db.password"),
+		DBname:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		logrus.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(task.Server)
 	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-		log.Fatalf("error occured while running http server: %s", err.Error())
+		logrus.Fatalf("error occured while running http server: %s", err.Error())
 	}
 
 }
--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -1,5 +1,7 @@
 package repository
 
+import "github.com/jmoiron/sqlx"
+
 type Taskslist interface {
 }
 
@@ -7,6 +9,6 @@ type Repository struct {
 	Taskslist
 }
 
-func NewRepository() *Repository {
+func NewRepository(db *sqlx.DB) *Repository {
 	return &Repository{}
 }
